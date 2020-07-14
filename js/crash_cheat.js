3fbcb16dc26dbaba0d91517e7b44f847f24fb024
zhangshude <1782700307@qq.com>
2020-07-14 12:14:04 +0800

    Site updated: 2020-07-14 12:14:04

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,1 +1,16 @@
-"use strict";var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/joke.ico"),document.title="看不见我🙈~看不见我🙈~",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/favicon.ico"),document.title=" ( ๑•̀ㅂ•́) ✧被发现了～",titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/joke.ico");
+        document.title = '看不见我🙈~看不见我🙈~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = ' ( ๑•̀ㅂ•́) ✧被发现了～';
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
